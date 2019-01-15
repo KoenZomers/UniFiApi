b85b451dba196b3aeb9b46b18cc305404d2bf246
Koen Zomers <koen@zomers.eu>
2019-01-15 01:31:30 +0100

    Removed old library files

--- Library/Responses/BaseResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KoenZomers.UniFi.Api.Responses
-{
-    /// <summary>
-    /// Base class to allow grouping all classes that represent UniFi Controller response messages
-    /// </summary>
-    public abstract class BaseResponse
-    {
-    }
-}
